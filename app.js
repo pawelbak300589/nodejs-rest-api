5039a32ce5fd0426bfbcf70325e369d2e79601bd
Pawel Bak <pawelbak300589@gmail.com>
2020-08-27 09:04:18 +0100

    Add handling errors in graphQL

--- app.js
+++ app.js
@@ -42,7 +42,16 @@ app.use((req, res, next) => {
 app.use('/graphql', graphqlHTTP({
     schema: graphqlSchema,
     rootValue: graphqlResolver,
-    graphiql: true
+    graphiql: true,
+    formatError(err) {
+        if (!err.originalError) {
+            return err;
+        }
+        const data = err.originalError.data;
+        const message = err.message || 'An error occured.';
+        const code = err.originalError.code || 500;
+        return { message, status: code, data };
+    }
 }));
 
 app.use((error, req, res, next) => {
