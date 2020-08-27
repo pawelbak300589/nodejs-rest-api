413a06dcf4a383fe72b44e4c2776e92245b2b066
Pawel Bak <pawelbak300589@gmail.com>
2020-08-27 15:41:57 +0100

    Add delete post functionality

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
@@ -10,6 +9,7 @@ const { graphqlHTTP } = require('express-graphql');
 const graphqlSchema = require('./graphql/schema');
 const graphqlResolver = require('./graphql/resolvers');
 const auth = require('./middleware/auth');
+const { clearImage } = require('./util/file');
 
 const app = express();
 
@@ -92,8 +92,3 @@ mongoose.connect(process.env.MONGODB_CLIENT_URL, { useNewUrlParser: true, useUni
         });
     })
     .catch(err => console.log(err));
-
-const clearImage = filePath => {
-    filePath = path.join(__dirname, '..', filePath);
-    fs.unlink(filePath, err => console.log(err));
-};
